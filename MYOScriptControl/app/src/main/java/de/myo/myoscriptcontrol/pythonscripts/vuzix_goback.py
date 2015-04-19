740d73b8c18c2bdb46b4b21c0f308489060ba246
FHe94 <felix.helfrich@web.de>
2015-04-19 15:01:54 +0200

    added output to testscripts

--- MYOScriptControl/app/src/main/java/de/myo/myoscriptcontrol/pythonscripts/vuzix_goback.py
+++ MYOScriptControl/app/src/main/java/de/myo/myoscriptcontrol/pythonscripts/vuzix_goback.py
@@ -1,6 +1,6 @@
 import android
 UUID = "457807c0-4897-11df-9879-0800200c9a66"
 droid = android.Android()
-droid.makeToast("MOVERIGHT")
+droid.makeToast("GOBACK")
 success = droid.bluetoothConnect(UUID, "98:DA:92:00:06:62")
-droid.bluetoothWrite("MOVERIGHT")
+droid.bluetoothWrite("GOBACK")
